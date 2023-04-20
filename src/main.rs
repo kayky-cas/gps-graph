8f2f5bc143872c4ae9b565f1c03efbf5f98fa329
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-04-19 21:27:04 -0300

    structure of the map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::HashSet;
+
+struct Link {
+    pos: Pos,
+    distance: f32,
+}
+
+#[derive(Clone, PartialEq)]
+struct Pos(f32, f32);
+
+struct Node {
+    pos: Pos,
+    neighs: Vec<Link>,
+}
+
+struct NavigationMap {
+    childs: HashSet<Pos, Node>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
